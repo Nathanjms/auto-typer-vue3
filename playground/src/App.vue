cd141764f7bed090a2c7272e48942c59624dfe3d
Nathan James <nathanjms@outlook.com>
2022-08-28 09:13:44 +0100

    Update demo

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { AutoTyperVue } from 'auto-typer-vue';
-
+import { AutoTyperVue } from "auto-typer-vue";
 </script>
 
 <template>
-  <AutoTyperVue text="test" />
+  <AutoTyperVue componentTag="h1" :text="['This is a demo.', 'And this is another Demo!']" />
 </template>
 
 <style scoped>
