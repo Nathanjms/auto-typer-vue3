4f0cb03bff0aa375aa23715a9821e277dd703c18
Nathan James <nathanjms@outlook.com>
2022-09-03 14:59:53 +0100

    Demo styling (tailwind)

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -1,18 +1,18 @@
 <script setup lang="ts">
 import { AutoTyperVue } from "auto-typer-vue";
 
-let text = [
-  'This is a demo.',
-  'And this is another Demo!'
-];
+let text = ["This is a demo.", "And this is another Demo!"];
 </script>
 
 <template>
-  <AutoTyperVue
-    beginningWord="Hello!&nbsp;"
-    componentTag="h1"
-    :text="text"
-    :startDelay="1500"
-    :typingDelay="150"
-  />
+  <div class="grid place-items-center justify-center min-h-screen bg-slate-700">
+    <AutoTyperVue
+      beginningWord="Hello!&nbsp;"
+      componentTag="h1"
+      :text="text"
+      :startDelay="1500"
+      :typingDelay="150"
+      class="text-gray-200"
+    />
+  </div>
 </template>
--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import InstallPlugin from 'auto-typer-vue';
 import '../../src/styles/main.scss';
