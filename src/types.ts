b25ead3dce969095f1af6fcdf6d5417b4e0252ef
Nathan James <nathanjms@outlook.com>
2022-09-09 21:23:49 +0100

    Types optional other than `text`

--- src/types.ts
+++ src/types.ts
@@ -1,13 +1,13 @@
 export interface VueAutoTyper {
-  componentTag: String;
-  customCursorStyling: Object;
-  beginningWord: String;
+  componentTag?: String;
+  customCursorStyling?: Object;
+  beginningWord?: String;
   text: String | Array<String>;
-  startDelay: Number;
-  betweenWordDelay: Number;
-  typingDelay: Number;
-  deletingDelay: Number;
-  waitBeforeDeleteDelay: Number;
-  startByDefault: Boolean;
-  repeat: Boolean;
+  startDelay?: Number;
+  betweenWordDelay?: Number;
+  typingDelay?: Number;
+  deletingDelay?: Number;
+  waitBeforeDeleteDelay?: Number;
+  startByDefault?: Boolean;
+  repeat?: Boolean;
 }
